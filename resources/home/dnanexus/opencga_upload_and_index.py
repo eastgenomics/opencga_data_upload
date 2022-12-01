832e72ddbe55b04dac9114ca8f3ba74b3a5047d4
Yujin Kim <yujin.kim@hotmail.fr>
2022-12-01 15:18:25 +0000

    handle metadata and not metadata

--- resources/home/dnanexus/opencga_upload_and_index.py
+++ resources/home/dnanexus/opencga_upload_and_index.py
@@ -86,20 +86,30 @@ if __name__ == '__main__':
     opencga_cli = args.cli
 
     # Check if metadata has been provided
-    metadata = None
     project = args.project
     study = args.study
+
+    vcf_data = {}
+
     if args.metadata is not None:
-        metadata = True
         logger.info("Metadata file provided: {}".format(args.metadata))
         # Link vcfs and metadata together
         vcf_with_metadata = link_metadata_vcfs(args.metadata, args.vcf)
+
+        for vcf_file, metadata_file in vcf_with_metadata:
+            manifest, samples, individuals, clinical = read_metadata(metadata_file=metadata_file, logger=logger)
+            vcf_data[vcf_file]["project"] = manifest['configuration']['projectId']
+            vcf_data[vcf_file]["study"] = manifest['study']['id']
+            vcf_data[vcf_file]["study_fqn"] = f"{manifest['configuration']['projectId']}:{manifest['study']['id']}"
     else:
-        metadata = False
         logger.info("No metadata has been provided, VCF will not be associated to any individuals or cases")
         if args.project is not None and args.study is not None:
+            for vcf_file in args.vcf:
+                vcf_data[vcf_file]["project"] = project
+                vcf_data[vcf_file]["study"] = study
+                vcf_data[vcf_file]["study_fqn"] = f"{project}:{study}"
+
             logger.info("Data will be loaded in study: {}:{}".format(project, study))
-            study_fqn = project + ":" + study
         else:
             logger.error("No project or study provided. Please provide a metadata file or specify the project and "
                          "study where data needs to be loaded.")
@@ -123,166 +133,151 @@ if __name__ == '__main__':
     somatic = args.somatic
     multi_file = args.multifile
 
-    # Read metadata file
-    if metadata:
-        for vcf_file, metadata_file in vcf_with_metadata:
-            manifest, samples, individuals, clinical = read_metadata(metadata_file=metadata_file, logger=logger)
-            '''
-            Overwrite project and study to point to the test project
-            TO BE CHANGED
-            '''
-            # manifest['configuration']['projectId'] = 'dnanexus'
-            # manifest['study']['id'] = 'app_test'
-            project = manifest['configuration']['projectId']
-            study = manifest['study']['id']
-            project = 'dnanexus'
-            study = 'app_test'
+    # Get case priority. If case priority is URGENT, jobs will not be delayed
+    delay = True
+    priority = clinical[0]['priority']['id']
+    if priority in no_delay_priority:
+        delay = False
 
-            # Get case priority. If case priority is URGENT, jobs will not be delayed
-            delay = True
-            priority = clinical[0]['priority']['id']
-            if priority in no_delay_priority:
-                delay = False
+    if clinical[0]['type'] == 'CANCER':
+        multi_file = True
 
-            if clinical[0]['type'] == 'CANCER':
-                multi_file = True
+    # Format DNA Nexus file ID to attributes
+    file_data = {}
+    file_data["attributes"] = {
+        "DNAnexusFileId": args.dnanexus_fid
+    }
 
-            # Format DNA Nexus file ID to attributes
-            file_data = {}
-            file_data["attributes"] = {
-                "DNAnexusFileId": args.dnanexus_fid
-            }
+    # define software
+    if 'tnhaplotyper2' in os.path.basename(vcf_file):
+        file_data['software'] = {'name': 'TNhaplotyper2'}
+    if '.flagged.' in os.path.basename(vcf_file):
+        file_data['software'] = {'name': 'Pindel'}
+    if '.SV.' in os.path.basename(vcf_file):
+        file_data['software'] = {'name': 'Manta'}
+    if os.path.basename(vcf_file).startswith('EH_'):
+        file_data['software'] = {'name': 'ExpansionHunter'}
 
-            # define software
-            if 'tnhaplotyper2' in os.path.basename(vcf_file):
-                file_data['software'] = {'name': 'TNhaplotyper2'}
-            if '.flagged.' in os.path.basename(vcf_file):
-                file_data['software'] = {'name': 'Pindel'}
-            if '.SV.' in os.path.basename(vcf_file):
-                file_data['software'] = {'name': 'Manta'}
-            if os.path.basename(vcf_file).startswith('EH_'):
-                file_data['software'] = {'name': 'ExpansionHunter'}
+    # Check the status of the file and execute the necessary actions
+    uploaded, indexed, annotated, sample_index, existing_file_path, sample_ids = check_file_status(oc=oc,
+                                                                                        study=study_fqn,
+                                                                                        file_name=os.path.basename(vcf_file),
+                                                                                        file_info=file_data,
+                                                                                        logger=logger, check_attributes=True)
 
-            # Check the status of the file and execute the necessary actions
-            uploaded, indexed, annotated, sample_index, existing_file_path, sample_ids = check_file_status(oc=oc,
-                                                                                                study=study_fqn,
-                                                                                                file_name=os.path.basename(vcf_file),
-                                                                                                file_info=file_data,
-                                                                                                logger=logger, check_attributes=True)
+    # UPLOAD
+    if uploaded:
+        logger.info("File {} already exists in the OpenCGA study {}. "
+                    "Path to file: {}".format(os.path.basename(vcf_file), study_fqn, existing_file_path))
+    else:
+        logger.info("Uploading file {} into study {}...".format(os.path.basename(vcf_file), study_fqn))
+        upload_file(opencga_cli=opencga_cli, oc=oc, study=study_fqn, file=vcf_file, file_path=file_path,
+                    file_info=file_data, logger=logger)
 
-            # UPLOAD
-            if uploaded:
-                logger.info("File {} already exists in the OpenCGA study {}. "
-                            "Path to file: {}".format(os.path.basename(vcf_file), study_fqn, existing_file_path))
-            else:
-                logger.info("Uploading file {} into study {}...".format(os.path.basename(vcf_file), study_fqn))
-                upload_file(opencga_cli=opencga_cli, oc=oc, study=study_fqn, file=vcf_file, file_path=file_path,
-                            file_info=file_data, logger=logger)
+    # INDEXING
+    if indexed:
+        logger.info("File {} is indexed in the OpenCGA study {}.".format(os.path.basename(vcf_file), study_fqn))
+    else:
+        logger.info("Indexing file {} into study {}...".format(os.path.basename(vcf_file), study_fqn))
+        index_file(oc=oc, study=study_fqn, file=os.path.basename(vcf_file), logger=logger,
+                somatic=somatic, multifile=multi_file)
 
-            # INDEXING
-            if indexed:
-                logger.info("File {} is indexed in the OpenCGA study {}.".format(os.path.basename(vcf_file), study_fqn))
-            else:
-                logger.info("Indexing file {} into study {}...".format(os.path.basename(vcf_file), study_fqn))
-                index_file(oc=oc, study=study_fqn, file=os.path.basename(vcf_file), logger=logger,
-                        somatic=somatic, multifile=multi_file)
+    # Launch variant stats index
+    logger.info("Launching variant stats...")
+    vsi_job = variant_stats_index(oc=oc, study=study_fqn, cohort='ALL', logger=logger)
+    # TODO: Check status of this job at the end
 
-            # Launch variant stats index
-            logger.info("Launching variant stats...")
-            vsi_job = variant_stats_index(oc=oc, study=study_fqn, cohort='ALL', logger=logger)
-            # TODO: Check status of this job at the end
+    # ANNOTATION
+    if annotated:
+        logger.info("File {} is already annotated in the OpenCGA study {}.".format(os.path.basename(vcf_file),
+                                                                                study_fqn))
+    else:
+        logger.info("Annotating file {} into study {}...".format(os.path.basename(vcf_file), study_fqn))
+        annotate_variants(oc=oc, project=project, study=study, logger=logger, delay=delay)
 
-            # ANNOTATION
-            if annotated:
-                logger.info("File {} is already annotated in the OpenCGA study {}.".format(os.path.basename(vcf_file),
-                                                                                        study_fqn))
-            else:
-                logger.info("Annotating file {} into study {}...".format(os.path.basename(vcf_file), study_fqn))
-                annotate_variants(oc=oc, project=project, study=study, logger=logger, delay=delay)
+    # Launch sample stats index
+    logger.info("Launching sample stats...")
+    # svs_job = sample_variant_stats(oc=oc, study=study_fqn, sample_ids=sample_ids, logger=logger)
+    # TODO: Check status of this job at the end
 
-            # Launch sample stats index
-            logger.info("Launching sample stats...")
-            # svs_job = sample_variant_stats(oc=oc, study=study_fqn, sample_ids=sample_ids, logger=logger)
-            # TODO: Check status of this job at the end
+    # SECONDARY ANNOTATION INDEX
+    secondary_annotation_index(oc=oc, study=study_fqn, logger=logger, delay=delay)
 
-            # SECONDARY ANNOTATION INDEX
-            secondary_annotation_index(oc=oc, study=study_fqn, logger=logger, delay=delay)
+    logger.info("Loading metadata...")
+    # LOAD TEMPLATE
+    # load_template(oc=oc, study=manifest['study']['id'], template=args.metadata,
+    #               logger=logger)
 
-            logger.info("Loading metadata...")
-            # LOAD TEMPLATE
-            # load_template(oc=oc, study=manifest['study']['id'], template=args.metadata,
-            #               logger=logger)
+    # CREATE IND
+    # Get sample ID
+    sampleIds = oc.files.info(study=study_fqn, files=os.path.basename(vcf_file), include="sampleIds").get_result(0)['sampleIds']
+    if len(sampleIds) >= 1 and 'TA2_S59_L008_tumor' in sampleIds:
+        sampleIds.remove('TA2_S59_L008_tumor')
+    if len(sampleIds) < 1:
+        logger.error("Unexpected number of samples in the VCF")
+        sys.exit(1)
 
-            # CREATE IND
-            # Get sample ID
-            sampleIds = oc.files.info(study=study_fqn, files=os.path.basename(vcf_file), include="sampleIds").get_result(0)['sampleIds']
-            if len(sampleIds) >= 1 and 'TA2_S59_L008_tumor' in sampleIds:
-                sampleIds.remove('TA2_S59_L008_tumor')
-            if len(sampleIds) < 1:
-                logger.error("Unexpected number of samples in the VCF")
-                sys.exit(1)
+    # Define individual
+    individual_id = individuals['id']
+    ind_data = {
+        'id': individual_id,
+        'name': individual_id,
+        'disorders': [{
+            'id': 'HaemOnc'
+        }],
+        'sex': individuals['sex']
+    }
+    # Check if individual exists
+    logger.info("Checking if individual exists...")
+    check_individual = oc.individuals.search(study=study_fqn, id=individual_id).get_num_results()
+    if check_individual == 0:
+        logger.info("Creating new individual {}...".format(individual_id))
+        oc.individuals.create(study=study_fqn, samples='{}'.format(",".join(sampleIds)), data=ind_data)
+    elif check_individual > 0:
+        logger.info("Individual {} already exists in the database. No action needed.".format(individual_id))
+        # oc.individuals.update(study=manifest['study']['id'], individuals=individual_id, data=ind_data,
+        #                       samples='{}'.format(",".join(sampleIds)), samples_action='ADD')
+    # associate sample and individual
+    for sampleID in sampleIds:
+        oc.samples.update(study=study_fqn, samples=sampleID, data={'individualId': individuals['id'],
+                                                                'somatic': somatic})
 
-            # Define individual
-            individual_id = individuals['id']
-            ind_data = {
-                'id': individual_id,
-                'name': individual_id,
-                'disorders': [{
-                    'id': 'HaemOnc'
-                }],
-                'sex': individuals['sex']
-            }
-            # Check if individual exists
-            logger.info("Checking if individual exists...")
-            check_individual = oc.individuals.search(study=study_fqn, id=individual_id).get_num_results()
-            if check_individual == 0:
-                logger.info("Creating new individual {}...".format(individual_id))
-                oc.individuals.create(study=study_fqn, samples='{}'.format(",".join(sampleIds)), data=ind_data)
-            elif check_individual > 0:
-                logger.info("Individual {} already exists in the database. No action needed.".format(individual_id))
-                # oc.individuals.update(study=manifest['study']['id'], individuals=individual_id, data=ind_data,
-                #                       samples='{}'.format(",".join(sampleIds)), samples_action='ADD')
-            # associate sample and individual
-            for sampleID in sampleIds:
-                oc.samples.update(study=study_fqn, samples=sampleID, data={'individualId': individuals['id'],
-                                                                        'somatic': somatic})
+    # CREATE CASE
+    logger.info("Checking if clinical case exists...")
+    clinical_case = {
+        'id': clinical[0]['id'],
+        'type': clinical[0]['type'],
+        'proband': {
+            'id': individuals['id'],
+            'samples': [{
+                'id': samples[0]['id'],
+            }]
+        },
+        'disorder': clinical[0]['disorder'],
+        'panels': [{'id': 'myeloid_genes'}],
+        'priority': clinical[0]['priority'],
+        'comments': [{
+            'message': 'Case created automatically',
+            'tags': ['auto', 'validation']
+        }],
+        'status': {'id': 'READY_FOR_INTERPRETATION'}
+    }
+    check_case = oc.clinical.search(study=study_fqn, id=clinical_case["id"]).get_num_results()
+    if check_case == 0:
+        logger.info("Creating new clinical case {}...".format(clinical_case["id"]))
+        oc.clinical.create(data=clinical_case, study=study_fqn, createDefaultInterpretation=True)
+    elif check_case == 1:
+        logger.info("Case {} already exists in the database.".format(clinical_case["id"]))
 
-            # CREATE CASE
-            logger.info("Checking if clinical case exists...")
-            clinical_case = {
-                'id': clinical[0]['id'],
-                'type': clinical[0]['type'],
-                'proband': {
-                    'id': individuals['id'],
-                    'samples': [{
-                        'id': samples[0]['id'],
-                    }]
-                },
-                'disorder': clinical[0]['disorder'],
-                'panels': [{'id': 'myeloid_genes'}],
-                'priority': clinical[0]['priority'],
-                'comments': [{
-                    'message': 'Case created automatically',
-                    'tags': ['auto', 'validation']
-                }],
-                'status': {'id': 'READY_FOR_INTERPRETATION'}
-            }
-            check_case = oc.clinical.search(study=study_fqn, id=clinical_case["id"]).get_num_results()
-            if check_case == 0:
-                logger.info("Creating new clinical case {}...".format(clinical_case["id"]))
-                oc.clinical.create(data=clinical_case, study=study_fqn, createDefaultInterpretation=True)
-            elif check_case == 1:
-                logger.info("Case {} already exists in the database.".format(clinical_case["id"]))
+    # SECONDARY SAMPLE INDEX
+    secondary_sample_index(oc=oc, study=study_fqn, sample=sampleIds[0], logger=logger)
 
-            # SECONDARY SAMPLE INDEX
-            secondary_sample_index(oc=oc, study=study_fqn, sample=sampleIds[0], logger=logger)
-
-            # Check again the status of the file
-            uploaded, indexed, annotated, sample_index, existing_file_path, sample_ids = check_file_status(oc=oc,
-                                                                                                study=study_fqn,
-                                                                                                file_name=os.path.basename(vcf_file),
-                                                                                                file_info=file_data,
-                                                                                                logger=logger, check_attributes=True)
+    # Check again the status of the file
+    uploaded, indexed, annotated, sample_index, existing_file_path, sample_ids = check_file_status(oc=oc,
+                                                                                        study=study_fqn,
+                                                                                        file_name=os.path.basename(vcf_file),
+                                                                                        file_info=file_data,
+                                                                                        logger=logger, check_attributes=True)
 
     # close loggers
     oh.close()
